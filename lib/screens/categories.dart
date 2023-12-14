dfb86b4dcc14ad27a9b88b100ba7b1678c383f86
karim-coder <ab.amiri484@gmail.com>
2023-12-14 14:32:23 +0530

    Provider added managing states

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,11 +6,8 @@ import 'package:meals/widgets/category_grid_item.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen(
-      {super.key,
-      required this.onToggleFavorite,
-      required this.availableMeals});
-  final void Function(Meal meal) onToggleFavorite;
+  const CategoriesScreen({super.key, required this.availableMeals});
+
   final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
@@ -20,9 +17,9 @@ class CategoriesScreen extends StatelessWidget {
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-            title: category.title,
-            meals: filteredMeals,
-            onToggleFavorite: onToggleFavorite),
+          title: category.title,
+          meals: filteredMeals,
+        ),
       ),
     ); //Navigator.push(context, '/category');
   }
